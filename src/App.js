175c4edb13d4221116afeb5cc7098eeeafdcaa87
Brandon-Schefstad <bschefstad@gmail.com>
2022-11-15 21:16:44 -0500

    Pure React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+import 'App.css';
+const App = () => {
+	return (
+		<div>
+			<h1>Updating app</h1>
+			<img src="./src/test.jpg"></img>
+		</div>
+	);
+};
+
+export default App;
